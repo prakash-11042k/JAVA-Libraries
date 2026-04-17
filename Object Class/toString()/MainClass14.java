72f5519931dd85d9aea09f019de6ad2fb0d03baf
prakash <prakash11042k@gmail.com>
2026-04-17 18:45:29 +0530

    toString() Example

---
+++ Object Class/toString()/MainClass14.java
@@ -0,0 +1,13 @@
+package Com;
+
+public class MainClass14 {
+
+	public static void main(String[] args) {
+		
+		Student6 s1=new Student6("Prasanna");
+		Student6 s2=new Student6("Venki");
+		
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+}
---
+++ Object Class/toString()/Student6.java
@@ -0,0 +1,17 @@
+package Com;
+
+public class Student6 {
+
+	String name;
+	
+	public Student6(String name) {
+		this.name=name;
+	}
+
+	@Override
+	public String toString() {
+		return  "name=" + name;
+	}
+	
+	
+}
