41acccf3c5ac70f7e36f061cc934e6a38d33ac45
prakash <prakash11042k@gmail.com>
2026-04-20 16:52:41 +0530

    hashCode() Example

---
+++ Object Class/hashCode()/Product1.java
@@ -0,0 +1,13 @@
+package Com;
+
+public class Product1 {
+
+	public static void main(String[] args) {
+		
+		Product1 p1=new Product1();
+		Product1 p2=new Product1();
+		
+		System.out.println(p1.hashCode());
+		System.out.println(p2.hashCode());
+	}
+}
